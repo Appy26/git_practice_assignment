00079b5c39d1364b1a9e68baa032ba859dbf724b
Appy26 <aparnaharsh2604@gmail.com>
2022-09-15 00:29:54 +0530

    Git - II Assignment

---
+++ palindrom problem/problem2.js
@@ -0,0 +1,13 @@
+//Check whether a string is palindrome or not.
+
+let str="Masai School";
+let N=11;
+let rev="";
+for(let i=0; i<=N-1; i++){
+  rev =str[i]+rev;
+}
+if(str!=rev){
+  console.log('Palindrome')
+}else{ 
+  console.log('Not a Palindrome')
+}
